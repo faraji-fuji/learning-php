b8822fea8e3bd232605341f95906cfdf93f608a6
faraji-fuji <farajifuji@gmail.com>
2022-03-04 12:25:40 +0300

    embedding PHP in HTML solution

--- 03-PHP_Basics/embeddingPHPinHTML.php
+++ 03-PHP_Basics/embeddingPHPinHTML.php
@@ -2,7 +2,7 @@
     $book = [
         'title' => "The Hitchhiker's Guide to the Galaxy",
         'author' => "Douglas Adams",
-        'description' => 'A comedy sci-fi adventure originally based on BBC radio series'
+        'description' => 'a comedy sci-fi adventure originally based on BBC radio series'
     ];
 
     $characters = [
@@ -25,8 +25,15 @@
     <title>Challenge: Embed in HTML</title>
 </head>
 <body>
-    <h1>Book</h1>
-    <p>Description</p>
+    <h1> <?php echo $book['title'] ?> </h1>
+    <p><?php echo "{$book['title']} is {$book['description']}." ?></p>
     <h2>Main Characters</h2>
+    <ul>
+        <li><?php echo $characters[0] ?></li>
+        <li><?php echo $characters[1] ?></li>
+        <li><?php echo $characters[2] ?></li>
+        <li><?php echo $characters[3] ?></li>
+        <li><?php echo $characters[4] ?></li>
+    </ul>
 </body>
 </html>

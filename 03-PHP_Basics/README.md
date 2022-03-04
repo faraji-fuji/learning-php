b4d4820dd5466aff1ed1a2630037b0f59775382b
faraji-fuji <farajifuji@gmail.com>
2022-03-04 11:51:59 +0300

    Initial Project Setup

---
+++ 03-PHP_Basics/README.md
@@ -0,0 +1,3 @@
+Use the arrays to create the contents of a webpage.
+Embed PHP in the HTML to create the main heading and the first paragraph.
+Bellow the second heading, create an unordered list to display the names of the five main characters
---
+++ 03-PHP_Basics/embeddingPHPinHTML.php
@@ -0,0 +1,32 @@
+<?php
+    $book = [
+        'title' => "The Hitchhiker's Guide to the Galaxy",
+        'author' => "Douglas Adams",
+        'description' => 'A comedy sci-fi adventure originally based on BBC radio series'
+    ];
+
+    $characters = [
+        'Arthur Dent',
+        'Ford Prefect',
+        'Zaphod Beeblebrox',
+        'Marvin, the paranoid android',
+        'Slartibartfast'
+    ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge: Embed in HTML</title>
+</head>
+<body>
+    <h1>Book</h1>
+    <p>Description</p>
+    <h2>Main Characters</h2>
+</body>
+</html>

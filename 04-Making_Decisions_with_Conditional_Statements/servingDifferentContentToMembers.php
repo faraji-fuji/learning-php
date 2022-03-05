b35a1cc4774f0f15898d98c211e04f7de0a421fc
faraji-fuji <farajifuji@gmail.com>
2022-03-05 13:00:22 +0300

    Serving different content to members. solution

--- 04-Making_Decisions_with_Conditional_Statements/servingDifferentContentToMembers.php
+++ 04-Making_Decisions_with_Conditional_Statements/servingDifferentContentToMembers.php
@@ -1,3 +1,11 @@
+<!-- This code presents different web page content depending on the membership status-->
+
+<!-- hard code membership status in a variable -->
+<!-- either member or non-member -->
+<?php $status = "member" ?> 
+
+
+<!-- display default html -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,12 +24,18 @@
             </p>
         </header>
 
+        <!-- display non-member content only when the membership status evaluates to true, i.e is non-member -->
+        <?php if ($status == 'non-member'){?> 
         <section class="non-members">
             <h2>Become a member</h2>
             <p>Useful information for non-members. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem recusandae maiores repellat officiis illum, animi excepturi fugit sint minima laudantium?</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quia porro sapiente impedit placeat maxime, pariatur modi dignissimos exercitationem dolores.</p>
         </section>
+        <?php };?>
+        
 
+        <!-- display this content for members only -->
+        <?php if ($status == "member"){?>
         <section class="members">
             <h2>Member's News</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, commodi, molestias quo in dolores libero modi corporis veritatis eligendi explicabo facilis!</p>
@@ -32,6 +46,8 @@
                 <li>Lorem ipsum dolor sit amet consectetur.</li>
             </ul>
         </section>
+        <?php };?>
+
 
         <section>
             <div class='both'>
@@ -40,10 +56,15 @@
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum similique, eum quasi fugit laborum in.</p>
             </div>
 
+
+            <!-- display content for members only -->
+            <?php if ($status == 'member'){?> 
             <div class='members'>
                 <h3>Information for members only</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam deserunt molestias rerum aliquam fugit temporibus voluptates doloribus officiis.</p>
             </div>
+            <?php };?>
+
         </section>
 
         <footer>

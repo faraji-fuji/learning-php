a226a5841e09c902b8fde00107d2427adb2726fe
faraji-fuji <farajifuji@gmail.com>
2022-03-10 13:48:56 +0300

    Integer division solution

--- 05-Doing_Calculations_with_PHP/integerDivision.php
+++ 05-Doing_Calculations_with_PHP/integerDivision.php
@@ -1,9 +1,19 @@
 <?php
 
 //convert $total_minutes to hours and minutes
-//Initial Project setup
+//solution
+
 
 $total_minutes = 318;
 $minutes = $total_minutes % 60;
 
-echo "Time taken was ?? hours $minutes";
+$hours = (int)floor($total_minutes/60); // use floor() function to get float value, use (int) to cast result into integer 
+
+echo "Time taken was $hours hours $minutes minutes".'<br>';
+
+
+
+//alternative method to get hours
+$hours = ($total_minutes - $minutes) / 60 ; // subtract the remaining minutes from the total minutes to get a number divisible by 60
+echo "Time taken was $hours hours $minutes minutes".'<br>';
+?>
